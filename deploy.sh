26892dd7f868ec36c16869eb5e66bdc178f1fa1e
hagendaz123 <hagenfeldtjacob@gmail.com>
2025-10-22 11:55:25 +0200

    added new deploy script to ensure that the vm version matches the remote exactly

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+set -e  # Exit immediately if any command fails
 
-read -p "GitHub username: " USERNAME
-read -s -p "GitHub personal access token: " TOKEN
-echo
-git pull https://$USERNAME:$TOKEN@github.com/olosva/tatou
+BRANCH="main"  
+
+#read the GitHub credentials and construct the repo URL
+read -p "GitHub username: " GITHUB_USER
+read -s -p "GitHub Personal Access Token: " GITHUB_TOKEN
+
+REPO_URL="https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/olosva/tatou"
+)
+cd "$(dirname "$0")"
+
+# Fetch latest commits from remote
+git fetch origin "$BRANCH"
+
+# Force local branch to match remote exactly
+git reset --hard "origin/$BRANCH"
+
+# Rebuild and start Docker containers
 sudo docker compose up --build -d
-
-echo "Should be working"
